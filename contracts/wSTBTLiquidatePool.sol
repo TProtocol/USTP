edf4a3880dadbf977a469008055be10221e9cfe2
0xtbill <dgy@tprotocol.io>
2023-06-28 16:48:38 +0800

    chore: add wSTBT pool

---
+++ contracts/wSTBTLiquidatePool.sol
@@ -0,0 +1,315 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./interfaces/ICurve.sol";
+import "./interfaces/AggregatorInterface.sol";
+
+contract wWSTBTLiquidatePool {
+	using SafeERC20 for IERC20;
+	using SafeMath for uint256;
+
+	// used to redeem wstbt
+	address public mxpRedeemPool;
+
+	address public admin;
+	// ustpool
+	address public ustpool;
+	// wstbt address
+	IERC20 public wstbt;
+	// usdc address
+	IERC20 public usdc;
+	// WSTBT curve pool
+	// Mainnet: 0x892D701d94a43bDBCB5eA28891DaCA2Fa22A690b
+	ICurve curvePool;
+
+	// Used to calculate the fee base.
+	uint256 public constant FEE_COEFFICIENT = 1e8;
+	// Max fee rates can't over then 1%
+	uint256 public constant maxLiquidateFeeRate = FEE_COEFFICIENT / 100;
+	uint256 public constant maxLiquidateMXPFeeRate = FEE_COEFFICIENT / 100;
+	// liquidateFeeRate: 0.1% => 100000 (10 ** 5)
+	// liquidateFeeRate: 10% => 10000000 (10 ** 7)
+	// liquidateFeeRate: 100% => 100000000 (10 ** 8)
+	// It's used when call liquidate method.
+	uint256 public liquidateFeeRate;
+	uint256 public liquidateMXPFeeRate;
+	// Fee Collector, used to receive fee.
+	address public feeCollector;
+
+	// liquidation index.
+	uint256 public liquidationIndex;
+	// the time for liquidation.
+	uint256 public processPeriod;
+
+	struct LiquidationDetail {
+		uint256 id;
+		uint256 timestamp;
+		address user;
+		uint256 repayAmount;
+		uint256 receiveAmountAfterFee;
+		uint256 MXPFee;
+		uint256 protocolFee;
+		// False not withdraw, or True.
+		bool isDone;
+	}
+
+	// Mapping from liquidation index to liquidationDetail.
+	mapping(uint256 => LiquidationDetail) public liquidationDetails;
+
+	// mint threshold for underlying token
+	uint256 public mintThreshold;
+	// redeem threshold for WSTBT
+	uint256 public redeemThreshold;
+	// target price
+	int256 public targetPrice;
+	// priceFeed be using check USDC is pegged
+	AggregatorInterface public priceFeed;
+	// coins , [DAI, USDC, USDT]
+	// see https://etherscan.io/address/0x892D701d94a43bDBCB5eA28891DaCA2Fa22A690b#code
+	address[3] public coins;
+
+	event liquidateRequested(
+		uint256 id,
+		uint256 timestamp,
+		address indexed user,
+		uint256 repayAmount,
+		uint256 underlyingAmount,
+		uint256 receiveAmountAfterFee,
+		uint256 MXPFee,
+		uint256 protocolFee
+	);
+	event FinalizeLiquidation(
+		address indexed user,
+		uint256 amount,
+		uint256 protocolFee,
+		uint256 id
+	);
+
+	constructor(
+		address _admin,
+		address _ustpool,
+		address _mxpRedeemPool,
+		address _wstbt,
+		address _usdc,
+		address _priceFeed,
+		address[3] memory _coins
+	) {
+		require(_admin != address(0), "!_admin");
+		require(_ustpool != address(0), "!_ustpool");
+		require(_mxpRedeemPool != address(0), "!_mxpRedeemPool");
+		require(_wstbt != address(0), "!_wstbt");
+		require(_usdc != address(0), "!_usdc");
+		require(_priceFeed != address(0), "!_priceFeed");
+
+		admin = _admin;
+		ustpool = _ustpool;
+		mxpRedeemPool = _mxpRedeemPool;
+		wstbt = IERC20(_wstbt);
+		usdc = IERC20(_usdc);
+		priceFeed = AggregatorInterface(_priceFeed);
+		coins = _coins;
+	}
+
+	modifier onlyAdmin() {
+		require(msg.sender == admin, "only admin");
+		_;
+	}
+
+	/**
+	 * @dev to set the period of processing
+	 * @param _processPeriod the period of processing. it's second.
+	 */
+	function setProcessPeriod(uint256 _processPeriod) external onlyAdmin {
+		processPeriod = _processPeriod;
+	}
+
+	/**
+	 * @dev to set the collector of fee
+	 * @param _feeCollector the address of collector
+	 */
+	function setFeeCollector(address _feeCollector) external onlyAdmin {
+		require(_feeCollector != address(0), "!_feeCollector");
+		feeCollector = _feeCollector;
+	}
+
+	/**
+	 * @dev to set the rate of liquidate fee
+	 * @param _liquidateFeeRate the rate. it should be multiply 10**6
+	 */
+	function setLiquidateFeeRate(uint256 _liquidateFeeRate) external onlyAdmin {
+		require(
+			_liquidateFeeRate <= maxLiquidateFeeRate,
+			"Liquidate fee rate should be less than 1%."
+		);
+		liquidateFeeRate = _liquidateFeeRate;
+	}
+
+	/**
+	 * @dev to set the rate of MP redeem fee
+	 * @param _liquidateMXPFeeRate the rate. it should be multiply 10**6
+	 */
+	function setRedeemMXPFeeRate(uint256 _liquidateMXPFeeRate) external onlyAdmin {
+		require(
+			_liquidateMXPFeeRate <= maxLiquidateMXPFeeRate,
+			"redeem mxp fee rate should be less than 1%."
+		);
+		liquidateMXPFeeRate = _liquidateMXPFeeRate;
+	}
+
+	/**
+	 * @dev to set the redeem pool
+	 * @param _redeemPool the address of redeem pool
+	 */
+	function setRedeemPool(address _redeemPool) external onlyAdmin {
+		require(_redeemPool != address(0), "!_redeemPool");
+		mxpRedeemPool = _redeemPool;
+	}
+
+	/**
+	 * @dev to set the wstbt curve pool
+	 * @param _curvePool the address of curve pool
+	 */
+	function setCurvePool(address _curvePool) external onlyAdmin {
+		require(_curvePool != address(0), "!_curvePool");
+		curvePool = ICurve(_curvePool);
+	}
+
+	/**
+	 * @dev to set the redeem threshold
+	 * @param amount the amount of redeem threshold
+	 */
+	function setRedeemThreshold(uint256 amount) external onlyAdmin {
+		redeemThreshold = amount;
+	}
+
+	/**
+	 * @dev to set the price
+	 * @param _targetPrice the target price of usdc
+	 */
+	function setPegPrice(int256 _targetPrice) external onlyAdmin {
+		targetPrice = _targetPrice;
+	}
+
+	/**
+	 * @dev get the exchange amount out from curve
+	 * @param wstbtAmount amount of wstbt
+	 * @param j token of index for curve pool
+	 */
+	function getFlashLiquidateAmountOutFromCurve(
+		uint256 wstbtAmount,
+		int128 j
+	) public view returns (uint256) {
+		// From wstbt to others
+		return curvePool.get_dy_underlying(0, j, wstbtAmount);
+	}
+
+	/// @notice get price feed answer
+	/// @return The answer of price from priceFeed
+	function latestAnswer() public view returns (int256) {
+		return priceFeed.latestAnswer();
+	}
+
+	/**
+	 * @dev Transfer a give amout of wstbt to matrixport's mint pool
+	 * @param caller the address of liquidator
+	 * @param wstbtAmount the amout of wstbt
+	 */
+	function liquidateWSTBT(address caller, uint256 wstbtAmount) external {
+		require(msg.sender == ustpool, "unauthorized");
+		require(priceFeed.latestAnswer() >= targetPrice, "depeg");
+		require(wstbtAmount >= redeemThreshold, "less than redeemThreshold.");
+		wstbt.safeTransfer(mxpRedeemPool, wstbtAmount);
+
+		// convert to USDC amount.
+		uint256 underlyingAmount = wstbtAmount.div(1e12);
+
+		uint256 liquidateFeeAmount = underlyingAmount.mul(liquidateFeeRate).div(FEE_COEFFICIENT);
+		uint256 liquidateMXPFeeAmount = underlyingAmount.mul(liquidateMXPFeeRate).div(
+			FEE_COEFFICIENT
+		);
+		uint256 amountAfterFee = underlyingAmount.sub(liquidateFeeAmount).sub(
+			liquidateMXPFeeAmount
+		);
+
+		liquidationIndex++;
+		liquidationDetails[liquidationIndex] = LiquidationDetail({
+			id: liquidationIndex,
+			timestamp: block.timestamp,
+			user: caller,
+			repayAmount: wstbtAmount,
+			receiveAmountAfterFee: amountAfterFee,
+			MXPFee: liquidateMXPFeeAmount,
+			protocolFee: liquidateFeeAmount,
+			isDone: false
+		});
+
+		emit liquidateRequested(
+			liquidationIndex,
+			block.timestamp,
+			msg.sender,
+			wstbtAmount,
+			underlyingAmount,
+			amountAfterFee,
+			liquidateMXPFeeAmount,
+			liquidateFeeAmount
+		);
+	}
+
+	/**
+	 * @dev Transfer a give amout of wstbt to matrixport's mint pool
+	 * @param wstbtAmount the amout of wstbt
+	 * @param j token of index for curve pool
+	 * @param minReturn the minimum amount of return
+	 * @param receiver used to receive token
+	 */
+	function flashLiquidateWSTBTByCurve(
+		uint256 wstbtAmount,
+		int128 j,
+		uint256 minReturn,
+		address receiver
+	) external {
+		require(msg.sender == ustpool, "unauthorized");
+		// From wstbt to others
+		uint256 dy = curvePool.get_dy_underlying(0, j, wstbtAmount);
+		require(dy >= minReturn, "!minReturn");
+		wstbt.approve(address(curvePool), wstbtAmount);
+		curvePool.exchange_underlying(0, j, wstbtAmount, dy);
+
+		// choose from coin list.
+		// Ensure that the transaction cannot succeed when the curve pool is maliciously replaced.
+		IERC20 targetToken = IERC20(coins[uint256(int256(j - 1))]);
+
+		uint256 feeAmount = dy.mul(liquidateFeeRate).div(FEE_COEFFICIENT);
+		uint256 amountAfterFee = dy.sub(feeAmount);
+		targetToken.safeTransfer(receiver, amountAfterFee);
+		targetToken.safeTransfer(feeCollector, feeAmount);
+	}
+
+	/**
+	 * @dev finalize liquidation
+	 * @param _id the id of liquidation details
+	 */
+	function finalizeLiquidationById(uint256 _id) external {
+		require(liquidationDetails[_id].user == msg.sender, "Not yours.");
+		require(liquidationDetails[_id].isDone == false, "Withdrawn");
+		require(
+			liquidationDetails[_id].timestamp + processPeriod <= block.timestamp,
+			"Not done yet."
+		);
+
+		uint256 redeemAmountAfterFee = liquidationDetails[_id].receiveAmountAfterFee;
+		uint256 protocolFee = liquidationDetails[_id].protocolFee;
+
+		liquidationDetails[_id].isDone = true;
+
+		// the MXP fee had been charge.
+		usdc.transfer(msg.sender, redeemAmountAfterFee);
+		usdc.transfer(feeCollector, protocolFee);
+
+		emit FinalizeLiquidation(msg.sender, redeemAmountAfterFee, protocolFee, _id);
+	}
+}
---
+++ contracts/wSTBTPool.sol
@@ -0,0 +1,532 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "./interfaces/IInterestRateModel.sol";
+import "./interfaces/IwSTBTLiquidatePool.sol";
+import "./nUSTP.sol";
+
+contract wWSTBTPool is nUSTP, AccessControl, Pausable {
+	using SafeMath for uint256;
+
+	bytes32 public constant POOL_MANAGER_ROLE = keccak256("POOL_MANAGER_ROLE");
+
+	uint256 public lastCheckpoint;
+	// Used to calculate the interest base.
+	uint256 public constant APR_COEFFICIENT = 1e8;
+	// Used to calculate the fee base.
+	uint256 public constant FEE_COEFFICIENT = 1e8;
+	// Used to calculate shares of WSTBT deposited by users.
+	uint256 public totalDepositedBalancesWSTBT;
+	// Used to calculate total supply of nUSTP.
+	uint256 public totalSupplynUSTP;
+
+	uint256 public safeCollateralRate = 101 * 1e18;
+	uint256 public reserveFactor;
+
+	// Used to record the user's WSTBT shares.
+	mapping(address => uint256) public depositedAmountWSTBT;
+	// Used to record the user's loan shares of nUSTP.
+	mapping(address => uint256) borrowedShares;
+	uint256 public totalBorrowShares;
+
+	// Used to be a flash liquidate provider
+	mapping(address => bool) flashLiquidateProvider;
+	mapping(address => bool) pendingFlashLiquidateProvider;
+
+	// We assume that the interest rate will not exceed 10%.
+	uint256 public constant maxInterestRate = APR_COEFFICIENT / 10;
+
+	// collateral token.
+	IERC20 public wstbt;
+	// Used to mint nUSTP.
+	IERC20 public usdc;
+	// interest rate model
+	IInterestRateModel public interestRateModel;
+	IwSTBTLiquidatePool public liquidatePool;
+
+	// the claimable fee for protocol
+	// reserves will be claim with nUSTP.
+	uint256 public totalUnclaimReserves;
+
+	event SupplyWSTBT(address indexed user, uint256 amount, uint256 shares, uint256 timestamp);
+	event SupplyUSDC(address indexed user, uint256 amount, uint256 timestamp);
+	event Mint(address indexed user, uint256 amount, uint256 timestamp);
+	event Burn(address indexed user, uint256 amount, uint256 timestamp);
+	event WithdrawWSTBT(address indexed user, uint256 amount, uint256 shares, uint256 timestamp);
+	event WithdrawUSDC(address indexed user, uint256 amount, uint256 timestamp);
+	event BorrowUSDC(address indexed user, uint256 amount, uint256 borrowShares, uint256 timestamp);
+	event RepayUSDC(address indexed user, uint256 amount, uint256 borrowShares, uint256 timestamp);
+
+	event ReservesAdded(uint256 addAmount, uint256 newTotalUnclaimReserves);
+	event LiquidationRecord(
+		address liquidator,
+		address indexed borrower,
+		uint256 nUSTPAmount,
+		uint256 timestamp
+	);
+
+	event SafeCollateralRateChanged(uint256 newSafeRatio);
+
+	// 0 is not, 1 is pending, 2 is a provider.
+	event FlashLiquidateProvider(address user, uint8 status);
+
+	constructor(
+		address admin,
+		IERC20 _wstbt,
+		IERC20 _usdc
+	) ERC20("Interest-bearing USD of TProtocol", "nUSTP") {
+		_setupRole(DEFAULT_ADMIN_ROLE, admin);
+		wstbt = _wstbt;
+		usdc = _usdc;
+	}
+
+	modifier realizeInterest() {
+		if (totalSupplynUSTP != 0) {
+			uint256 totalInterest = getRPS().mul(block.timestamp.sub(lastCheckpoint));
+			uint256 reserves = totalInterest.mul(reserveFactor).div(FEE_COEFFICIENT);
+
+			totalSupplynUSTP = totalSupplynUSTP.add(totalInterest).sub(reserves);
+			totalUnclaimReserves = totalUnclaimReserves.add(reserves);
+
+			emit ReservesAdded(reserves, totalUnclaimReserves);
+		}
+		lastCheckpoint = block.timestamp;
+		_;
+	}
+
+	/**
+	 * @notice Pause the contract. Revert if already paused.
+	 */
+	function pause() external onlyRole(DEFAULT_ADMIN_ROLE) {
+		_pause();
+	}
+
+	/**
+	 * @notice Unpause the contract. Revert if already unpaused.
+	 */
+	function unpause() external onlyRole(DEFAULT_ADMIN_ROLE) {
+		_unpause();
+	}
+
+	/**
+	 * @dev to set the liquidate pool
+	 * @param _address the address of liquidate pool
+	 */
+	function initLiquidatePool(
+		address _address
+	) external onlyRole(DEFAULT_ADMIN_ROLE) realizeInterest {
+		require(address(liquidatePool) == address(0), "initialized.");
+		liquidatePool = IwSTBTLiquidatePool(_address);
+	}
+
+	/**
+	 * @dev claim protocol's reserves fee
+	 * @param _receiver the address be used to receive reserves fee.
+	 */
+	function claimReservesFee(
+		address _receiver
+	) external realizeInterest onlyRole(DEFAULT_ADMIN_ROLE) {
+		_mintnUSTP(_receiver, totalUnclaimReserves);
+		totalUnclaimReserves = 0;
+	}
+
+	/**
+	 * @dev to set the rate of manager fee
+	 * @param _reserveFactor the rate. it should be multiply 10**6
+	 */
+	function setReserveFactor(
+		uint256 _reserveFactor
+	) external onlyRole(POOL_MANAGER_ROLE) realizeInterest {
+		require(_reserveFactor <= FEE_COEFFICIENT, "reserve factor should be less than 100%.");
+		reserveFactor = _reserveFactor;
+	}
+
+	/**
+	 * @dev to set interest rate model
+	 * @param _interestRateModel the model address
+	 */
+	function setInterestRateModel(
+		IInterestRateModel _interestRateModel
+	) external onlyRole(POOL_MANAGER_ROLE) realizeInterest {
+		// To ensure 100% utilization.
+		uint256 supplyInterestRate = _interestRateModel.getSupplyInterestRate(
+			totalSupplynUSTP,
+			totalSupplynUSTP
+		);
+		require(
+			supplyInterestRate <= maxInterestRate,
+			"interest rate should be less than maxInterestRate."
+		);
+		interestRateModel = _interestRateModel;
+	}
+
+	/**
+	 * @notice  safeCollateralRate
+	 */
+	function setSafeCollateralRate(
+		uint256 newSafeRatio
+	) external onlyRole(POOL_MANAGER_ROLE) realizeInterest {
+		require(newSafeRatio >= 101 * 1e18, "Safe CollateralRate should more than 101%");
+		safeCollateralRate = newSafeRatio;
+		emit SafeCollateralRateChanged(newSafeRatio);
+	}
+
+	/**
+	 * @notice Supply USDC.
+	 * Emits a `SupplyUSDC` event.
+	 *
+	 * @param _amount the amount of USDC
+	 */
+	function supplyUSDC(uint256 _amount) external realizeInterest whenNotPaused {
+		require(_amount > 0, "Supply USDC should more then 0.");
+		usdc.transferFrom(msg.sender, address(this), _amount);
+
+		// convert to nUSTP.
+		uint256 convertTonUSTP = _amount.mul(1e12);
+
+		_mintnUSTP(msg.sender, convertTonUSTP);
+
+		emit SupplyUSDC(msg.sender, _amount, block.timestamp);
+	}
+
+	/**
+	 * @notice Supply WSTBT.
+	 * Emits a `SupplyWSTBT` event.
+	 *
+	 * @param _amount the amount of WSTBT.
+	 */
+	function supplyWSTBT(uint256 _amount) external whenNotPaused realizeInterest {
+		require(_amount > 0, "Supply WSTBT should more then 0.");
+
+		wstbt.transferFrom(msg.sender, address(this), _amount);
+
+		totalDepositedBalancesWSTBT += _amount;
+		depositedAmountWSTBT[msg.sender] += _amount;
+
+		emit SupplyWSTBT(msg.sender, _amount, _amount, block.timestamp);
+	}
+
+	/**
+	 * @notice Withdraw WSTBT to an address.
+	 * Emits a `WithdrawWSTBT` event.
+	 *
+	 * @param _amount the amount of WSTBT.
+	 */
+	function withdrawWSTBT(uint256 _amount) external whenNotPaused realizeInterest {
+		require(_amount > 0, "Withdraw WSTBT should more then 0.");
+
+		totalDepositedBalancesWSTBT -= _amount;
+		depositedAmountWSTBT[msg.sender] -= _amount;
+
+		_requireIsSafeCollateralRate(msg.sender);
+		wstbt.transfer(msg.sender, _amount);
+
+		emit WithdrawWSTBT(msg.sender, _amount, _amount, block.timestamp);
+	}
+
+	/**
+	 * @notice Withdraw USDC to an address.
+	 * nUSTP:USDC always 1:1.
+	 * Emits a `WithdrawUSDC` event.
+	 *
+	 * @param _amount the amount of USDC.
+	 */
+	function withdrawUSDC(uint256 _amount) external whenNotPaused realizeInterest {
+		require(_amount > 0, "Withdraw USDC should more then 0.");
+
+		// convert to nUSTP.
+		uint256 convertTonUSTP = _amount.mul(10 ** 12);
+
+		_burnnUSTP(msg.sender, convertTonUSTP);
+		usdc.transfer(msg.sender, _amount);
+
+		emit WithdrawUSDC(msg.sender, _amount, block.timestamp);
+	}
+
+	/**
+	 * @notice Borrow USDC to an address.
+	 * Emits a `BorrowUSDC` event.
+	 *
+	 * @param _amount the amount of USDC.
+	 */
+	function borrowUSDC(uint256 _amount) external whenNotPaused realizeInterest {
+		require(_amount > 0, "Borrow USDC should more then 0.");
+
+		// convert to nUSTP.
+		uint256 convertTonUSTP = _amount.mul(10 ** 12);
+
+		uint256 borrowShares = getSharesBynUSTPAmount(convertTonUSTP);
+		borrowedShares[msg.sender] += borrowShares;
+		totalBorrowShares += borrowShares;
+
+		require(
+			getnUSTPAmountByShares(totalBorrowShares) <= totalSupplynUSTP,
+			"shold be less then supply of nUSTP."
+		);
+		_requireIsSafeCollateralRate(msg.sender);
+
+		usdc.transfer(msg.sender, _amount);
+
+		emit BorrowUSDC(msg.sender, _amount, borrowShares, block.timestamp);
+	}
+
+	/**
+	 * @notice Repay USDC from user
+	 * Emits a `RepayUSDC` event.
+	 *
+	 * @param _amount the amount of USDC.
+	 */
+	function repayUSDC(uint256 _amount) external whenNotPaused realizeInterest {
+		require(_amount > 0, "Repay USDC should more then 0.");
+
+		usdc.transferFrom(msg.sender, address(this), _amount);
+		// convert to nUSTP.
+		uint256 convertTonUSTP = _amount.mul(1e12);
+
+		uint256 repayShares = getSharesBynUSTPAmount(convertTonUSTP);
+		_repay(msg.sender, repayShares);
+
+		emit RepayUSDC(msg.sender, _amount, repayShares, block.timestamp);
+	}
+
+	/**
+	 * @notice Repay all USDC from user
+	 * Emits a `RepayUSDC` event.
+	 *
+	 */
+	function repayAll() external whenNotPaused realizeInterest {
+		uint256 userBorrowShares = borrowedShares[msg.sender];
+
+		uint256 repaynUSTP = getnUSTPAmountByShares(userBorrowShares);
+		// convert to USDC.
+		uint256 convertToUSDC = repaynUSTP.div(1e12) + 1;
+		usdc.transferFrom(msg.sender, address(this), convertToUSDC);
+		_repay(msg.sender, userBorrowShares);
+
+		emit RepayUSDC(msg.sender, convertToUSDC, userBorrowShares, block.timestamp);
+	}
+
+	/**
+	 * @notice The sender liquidates the borrowers collateral.
+	 * *Can be liquidated at any time*
+	 * Emits a `LiquidationRecord` event.
+	 *
+	 * @param borrower The borrower be liquidated
+	 * @param repayAmount The amount of the nUSTP to repay
+	 */
+	function liquidateBorrow(
+		address borrower,
+		uint256 repayAmount
+	) external whenNotPaused realizeInterest {
+		require(msg.sender != borrower, "don't liquidate self");
+		uint256 borrowedUSD = getnUSTPAmountByShares(borrowedShares[borrower]);
+		require(borrowedUSD >= repayAmount, "repayAmount should be less than borrower's debt.");
+		_burnnUSTP(msg.sender, repayAmount);
+
+		uint256 repayShares = getSharesBynUSTPAmount(repayAmount);
+
+		_repay(borrower, repayShares);
+
+		uint256 lqiuidateAmount = repayAmount.mul(1e18).div(_wstbtPrice());
+		// TODO maybe no need to check.
+		require(
+			depositedAmountWSTBT[borrower] >= lqiuidateAmount,
+			"lqiuidateAmount should be less than borrower's deposit."
+		);
+		totalDepositedBalancesWSTBT -= lqiuidateAmount;
+		depositedAmountWSTBT[borrower] -= lqiuidateAmount;
+
+		wstbt.transfer(address(liquidatePool), repayAmount);
+		liquidatePool.liquidateWSTBT(msg.sender, lqiuidateAmount);
+
+		emit LiquidationRecord(msg.sender, borrower, repayAmount, block.timestamp);
+	}
+
+	/**
+	 * @notice The sender liquidates the borrowers collateral by Curve.
+	 * *Can be liquidated at any time*
+	 * Emits a `LiquidationRecord` event.
+	 *
+	 * @param borrower The borrower be liquidated
+	 * @param repayAmount The amount of the nUSTP to repay
+	 * @param j token of index for curve pool
+	 * @param minReturn the minimum amount of return
+	 */
+	function flashLiquidateBorrow(
+		address borrower,
+		uint256 repayAmount,
+		int128 j,
+		uint256 minReturn
+	) external whenNotPaused realizeInterest {
+		require(flashLiquidateProvider[borrower], "borrower is not a provider.");
+		require(msg.sender != borrower, "don't liquidate self.");
+		uint256 borrowedUSD = getnUSTPAmountByShares(borrowedShares[borrower]);
+		require(borrowedUSD >= repayAmount, "repayAmount should be less than borrower's debt.");
+		_burnnUSTP(msg.sender, repayAmount);
+
+		uint256 repayShares = getSharesBynUSTPAmount(repayAmount);
+
+		_repay(borrower, repayShares);
+
+		uint256 lqiuidateAmount = repayAmount.mul(1e18).div(_wstbtPrice());
+		// TODO maybe no need to check.
+		require(
+			depositedAmountWSTBT[borrower] >= lqiuidateAmount,
+			"lqiuidateAmount should be less than borrower's deposit."
+		);
+		totalDepositedBalancesWSTBT -= lqiuidateAmount;
+		depositedAmountWSTBT[borrower] -= lqiuidateAmount;
+
+		wstbt.transfer(address(liquidatePool), repayAmount);
+		liquidatePool.flashLiquidateWSTBTByCurve(repayAmount, j, minReturn, msg.sender);
+
+		emit LiquidationRecord(msg.sender, borrower, repayAmount, block.timestamp);
+	}
+
+	/**
+	 * @notice User chooses to apply a provider
+	 */
+	function applyFlashLiquidateProvider() external {
+		pendingFlashLiquidateProvider[msg.sender] = true;
+		emit FlashLiquidateProvider(msg.sender, 1);
+	}
+
+	/**
+	 * @notice User chooses to cancel a provider
+	 */
+	function cancelFlashLiquidateProvider() external {
+		pendingFlashLiquidateProvider[msg.sender] = false;
+		flashLiquidateProvider[msg.sender] = false;
+		emit FlashLiquidateProvider(msg.sender, 0);
+	}
+
+	/**
+	 * @notice Admin accept a apply for provider
+	 */
+	function acceptFlashLiquidateProvider(address user) external onlyRole(POOL_MANAGER_ROLE) {
+		require(pendingFlashLiquidateProvider[user], "the user did not apply.");
+		pendingFlashLiquidateProvider[user] = false;
+		flashLiquidateProvider[user] = true;
+		emit FlashLiquidateProvider(user, 2);
+	}
+
+	/**
+	 * @notice Get the borrowed shares of user
+	 *
+	 * @param user the address of borrower
+	 */
+
+	function getBorrowedSharesOf(address user) external view returns (uint256) {
+		return borrowedShares[user];
+	}
+
+	/**
+	 * @notice Get the borrowed amount of user
+	 *
+	 * @param user the address of borrower
+	 */
+
+	function getBorrowedAmount(address user) external view returns (uint256) {
+		return getnUSTPAmountByShares(borrowedShares[user]);
+	}
+
+	/**
+	 * @dev mint nUSTP for _receiver.
+	 * Emits`Mint` and `Transfer` event.
+	 *
+	 * @param _receiver the address be used to receive nUSTP.
+	 * @param _amount the amount of nUSTP.
+	 */
+	function _mintnUSTP(address _receiver, uint256 _amount) internal {
+		uint256 sharesAmount = getSharesBynUSTPAmount(_amount);
+		if (sharesAmount == 0) {
+			//nUSTP shares are 1:1 to USDC at first.
+			sharesAmount = _amount;
+		}
+		_mintShares(_receiver, sharesAmount);
+		totalSupplynUSTP += _amount;
+		emit Mint(msg.sender, _amount, block.timestamp);
+		emit Transfer(address(0), _receiver, _amount);
+	}
+
+	/**
+	 * @dev burn nUSTP from _receiver.
+	 * Emits`Burn` and `Transfer` event.
+	 *
+	 * @param _account the address be used to burn nUSTP.
+	 * @param _amount the amount of nUSTP.
+	 */
+	function _burnnUSTP(address _account, uint256 _amount) internal {
+		uint256 sharesAmount = getSharesBynUSTPAmount(_amount);
+		require(sharesAmount > 0, "shares should be more then 0.");
+		_burnShares(_account, sharesAmount);
+		totalSupplynUSTP -= _amount;
+		emit Burn(msg.sender, _amount, block.timestamp);
+		emit Transfer(_account, address(0), _amount);
+	}
+
+	/**
+	 * @dev repay nUSTP for _account
+	 * Emits`Burn` and `Transfer` event.
+	 *
+	 * @param _account the address be usde to burn nUSTP.
+	 * @param _repayShares the amount of nUSTP shares.
+	 */
+	function _repay(address _account, uint256 _repayShares) internal {
+		borrowedShares[_account] -= _repayShares;
+		totalBorrowShares -= _repayShares;
+	}
+
+	/**
+	 * @notice total supply of nUSTP.
+	 */
+	function _getTotalSupplynUSTP() internal view override returns (uint256) {
+		return totalSupplynUSTP;
+	}
+
+	/**
+	 * @dev Return USD value of WSTBT
+	 * TODO Wait MXP's Oracle
+	 * it should be equal to $1.
+	 * maybe possible through the oracle.
+	 */
+	function _wstbtPrice() internal pure returns (uint256) {
+		return 1e18;
+	}
+
+	/**
+	 * @dev The USD value of the collateral asset must be higher than safeCollateralRate.
+	 */
+	function _requireIsSafeCollateralRate(address user) internal view {
+		uint256 borrowedAmount = getnUSTPAmountByShares(borrowedShares[user]);
+		if (borrowedAmount == 0) {
+			return;
+		}
+		require(
+			(depositedAmountWSTBT[user].mul(_wstbtPrice()).mul(100) / borrowedAmount) >=
+				safeCollateralRate,
+			"Cannot be lower than the safeCollateralRate."
+		);
+	}
+
+	/**
+	 * @dev revolutions per second
+	 */
+	function getRPS() public view returns (uint256) {
+		uint256 _totalSupplynUSTP = _getTotalSupplynUSTP();
+		uint256 supplyInterestRate = interestRateModel.getSupplyInterestRate(
+			_totalSupplynUSTP,
+			getnUSTPAmountByShares(totalBorrowShares)
+		);
+		require(
+			supplyInterestRate <= maxInterestRate,
+			"interest rate should be less than maxInterestRate."
+		);
+		return supplyInterestRate.mul(_totalSupplynUSTP).div(365 days).div(APR_COEFFICIENT);
+	}
+}
