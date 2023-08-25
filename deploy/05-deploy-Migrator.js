f7e7735888456d091f8a778b50bfec208ab61803
0xtbill <dgy@tprotocol.io>
2023-08-25 19:11:14 +0800

    chore: add migrator

--- deploy/05-deploy-Migrator.js
+++ deploy/05-deploy-Migrator.js
@@ -8,9 +8,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
 	const config = AddressConfig[network.config.chainId]
 
-	const wtbt = "0x7C92EC6E0b7e1fb3E2bBbCbf5ACE74C2b9bC9407"
-	const treasury = "0x07Ac55797D4F43f57cA92a49E65ca582cC287c27"
-	const borrower = "0xEAb746DE6bd1b2714ed95AaB6945B82315613264"
+	const wtbt = "0xD38e031f4529a07996aaB977d2B79f0e00656C56"
+	const treasury = "0xa01D9bc8343016C7DDD39852e49890a8361B2884"
+	const borrower = "0x7d273212AED9651797701a9dFb8e636F6Ba832b2"
 	const MigratorArgs = [
 		config.adminAddress,
 		config.rUSTPAddess,
